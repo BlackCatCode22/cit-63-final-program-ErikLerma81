1af9af73667e0adfe2f7dffa9ec76f6c14cc759d
ErikLerma81 <124100945+ErikLerma81@users.noreply.github.com>
2024-05-05 22:35:49 -0700

    Add files via upload

--- SearchComplexity.java
+++ SearchComplexity.java
@@ -2,15 +2,24 @@
 // Starter code for final programming assignment in CIT-63 Java Programming Spring 2024
 
 
+import javax.naming.NameNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class SearchComplexity {
 
     // Linear Search Method
     public static int linearSearch(int[] array, int target) {
+        long start = System.nanoTime();
+        int IterationCount = 1;
         for (int i = 0; i < array.length; i++) {
+            IterationCount++;
             if (array[i] == target) {
+                long end = System.nanoTime();
+                long time = end - start;
+                System.out.println(IterationCount - 1 + " total iterations to find target"  );
+                System.out.println("Nano time: " + time);
                 return i;  // Returns index of found element
             }
         }
@@ -19,35 +28,43 @@ public class SearchComplexity {
 
     // Binary Search Method
     public static int binarySearch(int[] array, int target) {
-        int left = 0;
-        int right = array.length - 1;
-        int iterations = 0;
+        long start = System.nanoTime();
+        return binarySearchRecursive(array, target, 0, array.length - 1,0, start);
+    }
 
-        while (left <= right) {
+    private static int binarySearchRecursive(int[] array, int target, int left, int right, int iterations, long starttime) {
+        if (left <= right) {
             int mid = left + (right - left) / 2;
             iterations++;
 
             if (array[mid] == target) {
-                System.out.println("Binary search iterations: " + iterations);
+                long end = System.nanoTime();
+                long time = end - starttime;
+                System.out.println(iterations + " total iteration to find target");
+                System.out.println("Nano time: " + time);
                 return mid;
-            }
-
-            if (array[mid] < target) {
-                left = mid + 1;
+            } else if (array[mid] < target) {
+                return binarySearchRecursive(array, target, mid + 1, right, iterations, starttime);
             } else {
-                right = mid - 1;
+                return binarySearchRecursive(array, target, left, mid - 1, iterations, starttime);
             }
         }
-
-        System.out.println("Binary search iterations: " + iterations);
         return -1;  // Target not found
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int n = 0;
+    int[] array = null;
+    int target = 0;
+
+    try {
         System.out.println("Enter number of elements in array:");
-        int n = scanner.nextInt();
-        int[] array = new int[n];
+        n = scanner.nextInt();
+        array = new int[n];
 
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
@@ -55,7 +72,16 @@ public class SearchComplexity {
         }
 
         System.out.println("Enter target number to search:");
-        int target = scanner.nextInt();
+        target = scanner.nextInt();
+
+    } catch (InputMismatchException e) {
+        System.out.println("Input error: Please enter valid integers.");
+        System.out.println("Please rerun the program again to proceed");
+        return; // Exit the program if there's an input error
+    }
+
+
+
 
         // Linear Search
         int linearResult = linearSearch(array, target);
@@ -71,5 +97,4 @@ public class SearchComplexity {
         scanner.close();
     }
 }
-
 
